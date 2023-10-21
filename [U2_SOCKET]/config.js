13fd6f25e17c34c361ccaf141b8507802e35fff3
cxzadm <cx.practica@gmail.com>
2023-10-20 22:12:07 -0500

    Modificaciones_Frontend

--- [U2_SOCKET]/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    PORT: 3001, 
-    DB_URL: 'mongodb+srv://batemp789:VNR4OOO5HmNj73UD@cluster0.53zhvqu.mongodb.net/?retryWrites=true&w=majority'
-
-}
-
-module.exports = config
--- [U2_SOCKET]/network/routes.js
+++
@@ -1,9 +0,0 @@
-const replegal = require('../components/replegal/interface')
-const empresa = require('../components/empresa/interface')
-
-const routes = function(server) {
-    server.use('/replegal', replegal)
-    server.use('/empresa', empresa)
-}
-
-module.exports = routes
