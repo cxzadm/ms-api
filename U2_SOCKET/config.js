6dc742332b648f73300bd737ff4d02011d47bb7e
cxzadm <cx.practica@gmail.com>
2023-11-12 22:57:55 -0500

    Agregando todos los cambios

--- U2_SOCKET/config.js
+++ U2_SOCKET/config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: 4001, 
-    DB_URL: 'mongodb+srv://batemp789:VNR4OOO5HmNj73UD@cluster0.53zhvqu.mongodb.net/?retryWrites=true&w=majority'
+    DB_URL: 'mongodb+srv://cxpractica:g5uaB6CI7eklTwEL@cluster0.rjf5sm6.mongodb.net/'
 }
 
 module.exports = config
